674720f63693b70b090b17d1de89851cc3b9e8c7
Joe Thorley <joe@poissonconsulting.ca>
2025-06-18 17:25:28 +0800

    Rename nsims to nsim

--- R/generate-data.R
+++ R/generate-data.R
@@ -2,7 +2,7 @@
 #'
 #' @param x The object to use for generating the data.
 #' @param nrow A integer vector of the number of rows in the generated data.
-#' @param nsims A count of the number of data sets to generate.
+#' @param nsim A count of the number of data sets to generate.
 #' @param ... Unused.
 #' @return A tibble of generate datasets.
 #' @export
@@ -12,9 +12,9 @@ ssd_generate_data <- function(x, ...) UseMethod("ssd_generate_data")
 #' @param replace A flag specifying whether to sample with replacement.
 #' @export
 #' @examples
-#' ssd_generate_data(ssddata::ccme_boron, nrow = 5, nsims = 3)
+#' ssd_generate_data(ssddata::ccme_boron, nrow = 5, nsim = 3)
 #' 
-ssd_generate_data.data.frame <- function(x, ..., replace = FALSE, nrow = 6L, nsims = 100L) {
+ssd_generate_data.data.frame <- function(x, ..., replace = FALSE, nrow = 6L, nsim = 100L) {
   chk::check_data(
     x, values = list(Conc = c(0,Inf,NA_real_)), nrow = c(5, 10000)
   )
@@ -23,23 +23,23 @@ ssd_generate_data.data.frame <- function(x, ..., replace = FALSE, nrow = 6L, nsi
   chk::chk_range(nrow, c(5, 1000))
   chk::chk_length(nrow, upper = 100)
   chk::chk_not_any_na(nrow)
-  chk::chk_count(nsims)
-  chk::chk_range(nsims, c(1, 10000))
+  chk::chk_count(nsim)
+  chk::chk_range(nsim, c(1, 10000))
   chk::chk_unused(...)
   
   if(length(nrow) == 1) {
-    data <- nsims |>
+    data <- nsim |>
       seq_len() |>
       purrr::map(\(n) dplyr::slice_sample(x, n = nrow, replace = replace)) |>
       purrr::map(\(.x) dplyr::mutate(.x, row = seq_len(nrow))) |>
-      purrr::map2(seq_len(nsims), \(.x, .y) dplyr::mutate(.x, sim = .y)) |>
+      purrr::map2(seq_len(nsim), \(.x, .y) dplyr::mutate(.x, sim = .y)) |>
       dplyr::bind_rows() |>
       dplyr::mutate(nrow = nrow,
                     replace = replace)
     return(data)
   }
   nrow |>
-    purrr::map(\(.x) ssd_generate_data(x, replace = replace, nrow = .x, nsims = nsims)) |>
+    purrr::map(\(.x) ssd_generate_data(x, replace = replace, nrow = .x, nsim = nsim)) |>
     dplyr::bind_rows()
 }
 
@@ -50,9 +50,9 @@ ssd_generate_data.data.frame <- function(x, ..., replace = FALSE, nrow = 6L, nsi
 #' @export
 #' @examples
 #' fit <- ssdtools::ssd_fit_dists(ssddata::ccme_boron)
-#' ssd_generate_data(fit, nrow = 5, nsims = 3)
+#' ssd_generate_data(fit, nrow = 5, nsim = 3)
 #' 
-ssd_generate_data.fitdists <- function(x, ..., dist = "top", nrow = 6L, nsims = 100L) {
+ssd_generate_data.fitdists <- function(x, ..., dist = "top", nrow = 6L, nsim = 100L) {
   chk::chk_string(dist)
   chk::chk_subset(dist, c("multi", "top", names(x)))
   chk::chk_unused(...)
@@ -67,30 +67,30 @@ ssd_generate_data.fitdists <- function(x, ..., dist = "top", nrow = 6L, nsims = 
     wch <- which.max(weight)
   }
   
-  ssd_generate_data(x[[wch]], nrow = nrow, nsims = nsims)
+  ssd_generate_data(x[[wch]], nrow = nrow, nsim = nsim)
 }
 
 #' @describeIn ssd_generate_data Generate data from tmbfit object
 #' @export
 #' @examples
 #' fit <- ssdtools::ssd_fit_dists(ssddata::ccme_boron)
-#' ssd_generate_data(fit[[1]], nrow = 5, nsims = 3)
+#' ssd_generate_data(fit[[1]], nrow = 5, nsim = 3)
 #' 
-ssd_generate_data.tmbfit <- function(x, ..., nrow = 6L, nsims = 100L) {
+ssd_generate_data.tmbfit <- function(x, ..., nrow = 6L, nsim = 100L) {
   chk::chk_unused(...)
   
   pars <- ssdtools::estimates(x)
   x <- x$dist
-  ssd_generate_data(x, pars = pars, nrow = nrow, nsims = nsims)
+  ssd_generate_data(x, pars = pars, nrow = nrow, nsim = nsim)
 }
 
 #' @describeIn ssd_generate_data Generate data using distribution name
 #' @param pars A named list of the parameter values.
 #' @export
 #' @examples
-#' ssd_generate_data("lnorm", nrow = 5, nsims = 3)
+#' ssd_generate_data("lnorm", nrow = 5, nsim = 3)
 #' 
-ssd_generate_data.character <- function(x, ..., pars = list(), nrow = 6L, nsims = 100L) {
+ssd_generate_data.character <- function(x, ..., pars = list(), nrow = 6L, nsim = 100L) {
   chk::chk_string(x)
   chk::chk_subset(x, ssdtools::ssd_dists_all())
   chk::chk_list(pars)
@@ -99,40 +99,40 @@ ssd_generate_data.character <- function(x, ..., pars = list(), nrow = 6L, nsims 
   fun <- paste0("ssdtools::ssd_r", x)
   fun <- eval(parse(text = fun))
   
-  ssd_generate_data(fun, args = as.list(pars), nrow = nrow, nsims = nsims)
+  ssd_generate_data(fun, args = as.list(pars), nrow = nrow, nsim = nsim)
 }
 
 #' @describeIn ssd_generate_data Generate data using function
 #' @param args A named list of the argument values.
 #' @export
 #' @examples
-#' ssd_generate_data(ssdtools::ssd_rlnorm, nrow = 5, nsims = 3)
+#' ssd_generate_data(ssdtools::ssd_rlnorm, nrow = 5, nsim = 3)
 #'
-ssd_generate_data.function <- function(x, ..., args = list(), nrow = 6L, nsims = 100L) {
+ssd_generate_data.function <- function(x, ..., args = list(), nrow = 6L, nsim = 100L) {
   chk::chk_list(args)
   chk::chk_whole_numeric(nrow)
   chk::chk_range(nrow, c(5, 1000))
   chk::chk_length(nrow, upper = 100)
   chk::chk_not_any_na(nrow)
-  chk::chk_count(nsims)
-  chk::chk_range(nsims, c(1, 10000))
+  chk::chk_count(nsim)
+  chk::chk_range(nsim, c(1, 10000))
   chk::chk_unused(...)
   
   if(length(nrow) == 1) {
     args$n <- nrow
     
-    data <- nsims |>
+    data <- nsim |>
       seq_len() |>
       purrr::map(\(n) do.call(x, args = args)) |>
       purrr::map(\(.x) dplyr::tibble(Conc = .x)) |>
       purrr::map(\(.x) dplyr::mutate(.x, row = seq_len(nrow))) |>
-      purrr::map2(seq_len(nsims), \(.x, .y) dplyr::mutate(.x, sim = .y)) |>
+      purrr::map2(seq_len(nsim), \(.x, .y) dplyr::mutate(.x, sim = .y)) |>
       dplyr::bind_rows() |>
       dplyr::mutate(nrow = nrow,
                     args = list(args))
     return(data)
   }
   nrow |>
-    purrr::map(\(.x) ssd_generate_data(x, args = args, nrow = .x, nsims = nsims)) |>
+    purrr::map(\(.x) ssd_generate_data(x, args = args, nrow = .x, nsim = nsim)) |>
     dplyr::bind_rows()
 }
--- tests/testthat/test-generate-data.R
+++ tests/testthat/test-generate-data.R
@@ -1,18 +1,18 @@
 test_that("ssd_generate_data.function works", {
   withr::local_seed(42)
-  data <- ssd_generate_data(ssdtools::ssd_rlnorm, nrow = 5, nsims = 10)
+  data <- ssd_generate_data(ssdtools::ssd_rlnorm, nrow = 5, nsim = 10)
   expect_snapshot_data(data, "function")
 })
 
 test_that("ssd_generate_data.function works multiple nrow", {
   withr::local_seed(42)
-  data <- ssd_generate_data(ssdtools::ssd_rlnorm, nrow = c(5,7), nsims = 2)
+  data <- ssd_generate_data(ssdtools::ssd_rlnorm, nrow = c(5,7), nsim = 2)
   expect_snapshot_data(data, "function2")
 })
 
 test_that("ssd_generate_data.character works", {
   withr::local_seed(42)
-  data <- ssd_generate_data("lnorm", nrow = 5, nsims = 10)
+  data <- ssd_generate_data("lnorm", nrow = 5, nsim = 10)
   expect_snapshot_data(data, "character")
 })
 
@@ -20,7 +20,7 @@ test_that("ssd_generate_data.fitdists works top", {
   data <- ssddata::ccme_boron
   fit <- ssdtools::ssd_fit_dists(data, dists = c("lnorm", "gamma"))
   withr::local_seed(42)
-  data <- ssd_generate_data(fit, dist = "lnorm", nrow = 5, nsims = 10)
+  data <- ssd_generate_data(fit, dist = "lnorm", nrow = 5, nsim = 10)
   expect_snapshot_data(data, "fitdists_top")
 })
 
@@ -28,7 +28,7 @@ test_that("ssd_generate_data.fitdists works top multiple", {
   data <- ssddata::ccme_boron
   fit <- ssdtools::ssd_fit_dists(data, dists = c("lnorm", "gamma"))
   withr::local_seed(42)
-  data <- ssd_generate_data(fit, dist = "top", nrow = c(5,7), nsims = 2)
+  data <- ssd_generate_data(fit, dist = "top", nrow = c(5,7), nsim = 2)
   expect_snapshot_data(data, "fitdists_top2")
 })
 
@@ -36,7 +36,7 @@ test_that("ssd_generate_data.fitdists works name", {
   data <- ssddata::ccme_boron
   fit <- ssdtools::ssd_fit_dists(data, dists = c("lnorm", "gamma"))
   withr::local_seed(42)
-  data <- ssd_generate_data(fit, dist = "gamma", nrow = 5, nsims = 10)
+  data <- ssd_generate_data(fit, dist = "gamma", nrow = 5, nsim = 10)
   expect_snapshot_data(data, "fitdists_name")
 })
 
@@ -44,18 +44,18 @@ test_that("ssd_generate_data.tmbfit works", {
   data <- ssddata::ccme_boron
   tmbfit <- ssdtools::ssd_fit_dists(data, dists = "lnorm")[[1]]
   withr::local_seed(42)
-  data <- ssd_generate_data(tmbfit, nrow = 5, nsims = 10)
+  data <- ssd_generate_data(tmbfit, nrow = 5, nsim = 10)
   expect_snapshot_data(data, "tmbfit")
 })
 
 test_that("ssd_generate_data.data.frame works", {
   withr::local_seed(42)
-  data <- ssd_generate_data(ssddata::ccme_boron, nrow = 5, nsims = 10)
+  data <- ssd_generate_data(ssddata::ccme_boron, nrow = 5, nsim = 10)
   expect_snapshot_data(data, "data.frame")
 })
 
 test_that("ssd_generate_data.data.frame works multiple nrow", {
   withr::local_seed(42)
-  data <- ssd_generate_data(ssddata::ccme_boron, nrow =c(5,7), nsims = 2)
+  data <- ssd_generate_data(ssddata::ccme_boron, nrow =c(5,7), nsim = 2)
   expect_snapshot_data(data, "data.frame2")
 })
