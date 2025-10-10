22f0ea6d0f5d0cbf6784e40624e9087124ed9575
Joe Thorley <joe@poissonconsulting.ca>
2025-10-10 09:11:06 -0700

    Rename ssd_get_streams_seeds() to ssd_get_seeds_streams()

--- R/seeds.R
+++ R/seeds.R
@@ -84,13 +84,13 @@ get_sub_seeds <- function(seed, start_seed, nseeds) {
 #'
 #' @examples
 #' withr::with_seed(10,
-#' sdd_get_streams_seeds(nseeds = 2, nstreams = 2)
+#' ssd_get_seeds_streams(nseeds = 2, nstreams = 2)
 #')
 #' withr::with_seed(10,
-#' sdd_get_streams_seeds(nseeds = 1, nstreams = 2, start_seed= 2)
+#' ssd_get_seeds_streams(nseeds = 1, nstreams = 2, start_seed= 2)
 #')
 # inspired by furrr:::generate_seed_streams
-sdd_get_streams_seeds <- function(seed = NULL, ..., nseeds = 1L, nstreams = 1L, start_seed = 1L, start_stream = 1L) {
+ssd_get_seeds_streams <- function(seed = NULL, ..., nseeds = 1L, nstreams = 1L, start_seed = 1L, start_stream = 1L) {
   chk::chk_null_or(seed, vld = chk::vld_whole_number)
   chk::chk_unused(...)
   chk::chk_count(nseeds)
--- tests/testthat/test-seeds.R
+++ tests/testthat/test-seeds.R
@@ -1,23 +1,23 @@
-test_that("sdd_get_streams_seeds no streams or seeds", {
-  expect_identical(sdd_get_streams_seeds(nstreams = 0L), list())
-  expect_identical(sdd_get_streams_seeds(nseeds = 0L), list(list()))
-  expect_identical(sdd_get_streams_seeds(nseeds = 0L, nstreams = 0L), list())
-  expect_identical(sdd_get_streams_seeds(nseeds = 0L, nstreams = 2L), list(list(),list()))
+test_that("ssd_get_seeds_streams no streams or seeds", {
+  expect_identical(ssd_get_seeds_streams(nstreams = 0L), list())
+  expect_identical(ssd_get_seeds_streams(nseeds = 0L), list(list()))
+  expect_identical(ssd_get_seeds_streams(nseeds = 0L, nstreams = 0L), list())
+  expect_identical(ssd_get_seeds_streams(nseeds = 0L, nstreams = 2L), list(list(),list()))
 })
 
-test_that("sdd_get_streams_seeds streams and start_seed", {
-  expect_snapshot(withr::with_seed(10, sdd_get_streams_seeds(nseeds = 2L)))
-  expect_snapshot(withr::with_seed(10, sdd_get_streams_seeds(nseeds = 2L, nstreams = 2L)))
-  expect_snapshot(withr::with_seed(10, sdd_get_streams_seeds(nseeds = 1L, nstreams = 2L, start_seed= 1L))) 
-  expect_identical(withr::with_seed(10, sdd_get_streams_seeds(nseeds = 2L, nstreams = 2L))[[2]][2],
-                   withr::with_seed(10, sdd_get_streams_seeds(nseeds = 1L, nstreams = 2L, start_seed= 2L))[[2]])
+test_that("ssd_get_seeds_streams streams and start_seed", {
+  expect_snapshot(withr::with_seed(10, ssd_get_seeds_streams(nseeds = 2L)))
+  expect_snapshot(withr::with_seed(10, ssd_get_seeds_streams(nseeds = 2L, nstreams = 2L)))
+  expect_snapshot(withr::with_seed(10, ssd_get_seeds_streams(nseeds = 1L, nstreams = 2L, start_seed= 1L))) 
+  expect_identical(withr::with_seed(10, ssd_get_seeds_streams(nseeds = 2L, nstreams = 2L))[[2]][2],
+                   withr::with_seed(10, ssd_get_seeds_streams(nseeds = 1L, nstreams = 2L, start_seed= 2L))[[2]])
 })
 
-test_that("sdd_get_streams_seeds advances seed by 1", {
+test_that("ssd_get_seeds_streams advances seed by 1", {
   withr::with_seed(10, {
     seed <- globalenv()$.Random.seed
     expect_snapshot(globalenv()$.Random.seed)
-    sdd_get_streams_seeds(nseeds = 2L)
+    ssd_get_seeds_streams(nseeds = 2L)
     seed2 <- globalenv()$.Random.seed 
     expect_snapshot(globalenv()$.Random.seed)
     set.seed(10)
@@ -28,7 +28,7 @@ test_that("sdd_get_streams_seeds advances seed by 1", {
   )
 })
 
-test_that("sdd_get_streams_seeds pass seed", {
- expect_snapshot(sdd_get_streams_seeds(10, nseeds = 2L))
- expect_identical(sdd_get_streams_seeds(10, nseeds = 2L), withr::with_seed(10, sdd_get_streams_seeds(10, nseeds = 2L)))
+test_that("ssd_get_seeds_streams pass seed", {
+ expect_snapshot(ssd_get_seeds_streams(10, nseeds = 2L))
+ expect_identical(ssd_get_seeds_streams(10, nseeds = 2L), withr::with_seed(10, ssd_get_seeds_streams(10, nseeds = 2L)))
 })
