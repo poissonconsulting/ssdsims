b4a618ece25bb97455cce146fc874cf8b0e5bf93
Joe Thorley <joe@poissonconsulting.ca>
2025-10-15 10:15:28 -0700

    update fit dists sims

--- R/fit-dists-sims.R
+++ R/fit-dists-sims.R
@@ -1,14 +1,14 @@
 
-fit_dists_sim <- function(data, i, dists, silent, save_to, return_items, ...) {
+fit_dists_sim <- function(data, seed, i, dists, silent, save_to, return_items, ...) {
   ## TODO: handle failure of all model to fit!!
   fit <- ssdtools::ssd_fit_dists(data, dists = dists, silent = silent, ...)
   if(chk::vld_string(save_to)) {
     saveRDS(fit, filepath(i, save_to, prefix = "fit", ext = ".rds"))
   }
-  if(return_items) {
-    return(fit)
+  if(!return_items) {
+    return(list())
   }
-  NULL
+  fit
 }
 
 #' Fit SSD Distributions to Simulated Data
@@ -20,8 +20,16 @@ fit_dists_sim <- function(data, i, dists, silent, save_to, return_items, ...) {
 ssd_fit_dists_sims <- function(datas, dists = ssdtools::ssd_dists_bcanz(), ..., silent = TRUE, save_to = NULL, return_items = TRUE, seed = NULL, stream = 1L, start_seed = 1L) {
   chk::chk_null_or(save_to, vld = chk::vld_dir)
   chk::chk_flag(return_items)
-  
-  ## TODO: local_set_seed_stream(seed = seed, start_seed = start_seed, start_stream = stream)
-  seq <- seq_len(length(datas))
-  purrr::map2(datas, seq, \(.x, .i) fit_dists_sim(.x, .i, dists = dists, silent = silent, save_to = save_to, return_items = return_items, ...))
+  chk::chk_list(datas)
+  if(!chk::vld_all(datas, chk::vld_data)) {
+    chk::chk_all(datas, chk::chk_data)
+  }
+  if(!length(datas)) {
+    return(list())
+  }
+
+  n <- length(datas)
+  seeds <- ssd_get_seeds_streams(seed = seed, nseeds = n, start_stream = stream, start_seed = start_seed)
+  seq <- start_seed:(start_seed + n - 1L)
+  purrr::pmap(list(datas, seeds, seq), \(.x, .s, .i) fit_dists_sim(.x, .s, .i, dists = dists, silent = silent, save_to = save_to, return_items = return_items, ...))
 }
