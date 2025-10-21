5c34655169b2601886efe95cdd6da861b4943e8f
Joe Thorley <joe@poissonconsulting.ca>
2025-10-20 18:17:14 -0700

    rename to lecuyer_cmrg_seed_stream

--- R/lecuyer-cmrg-seed.R
+++ R/lecuyer-cmrg-seed.R
@@ -1,3 +1,9 @@
+rinteger <- function(n = 1L) {
+  if (n == 0) integer(0)
+  mx <- 2147483647L
+  as.integer(stats::runif(n, -mx, mx))
+}
+
 # internal function from withr
 has_seed <- function () {
   exists(".Random.seed", globalenv(), mode = "integer", inherits = FALSE)
@@ -12,7 +18,6 @@ get_seed <- function ()
   list(random_seed = get(".Random.seed", globalenv(), mode = "integer", 
   inherits = FALSE), rng_kind = RNGkind())
 }
-
 
 # internal function from withr
 restore_rng_kind <- function (kind) 
@@ -58,3 +63,47 @@ with_lecuyer_cmrg_seed <- function(seed, code) {
     .rng_normal_kind = "Inversion",
     .rng_sample_kind = "Rejection")
 }
+
+get_lecuyer_cmrg_seed <- function() {
+  seed <- get_seed()
+  on.exit(set_seed(seed))
+  RNGkind("L'Ecuyer-CMRG", "Inversion", "Rejection")
+  set.seed(rinteger(1))
+  globalenv()$.Random.seed
+}
+
+# inspired by furrr:::generate_seed_streams
+get_lecuyer_cmrg_seed_stream <- function(seed = NULL, ..., nseeds = 1L, stream = 1L, start_seed = 1L) {
+  chk::chk_null_or(seed, vld = chk::vld_whole_number)
+  chk::chk_unused(...)
+  chk::chk_count(nseeds)
+  chk::chk_count(stream)
+  chk::chk_gt(stream)
+  chk::chk_count(start_seed)
+  chk::chk_gt(start_seed)
+
+  if(nseeds == 0) {
+    return(list())
+  }
+
+  oseed <- get_seed()
+  on.exit(set_seed(oseed))
+
+  if(!is.null(seed)) {
+    set.seed(seed)
+  }
+
+  seeds <- vector("list", length = nseeds)
+  seeds[[1]] <- parallel::nextRNGStream(get_lecuyer_cmrg_seed())
+  for(i in seq_len(stream - 1)) {
+    seeds[[1]] <- parallel::nextRNGStream(seeds[[1]])
+  }
+  for(i in seq_len(start_seed - 1)) {
+    seeds[[1]] <- parallel::nextRNGSubStream(seeds[[1]])
+  }
+  for(i in seq_len(nseeds-1)) {
+    seeds[[i+1]] <- parallel::nextRNGSubStream(seeds[[i]])
+  }
+  seeds
+}
+  
--- tests/testthat/_snaps/lecuyer-cmrg-seed.md
+++ tests/testthat/_snaps/lecuyer-cmrg-seed.md
@@ -1,17 +1,17 @@
-# get_seed_stream repeatable
+# get_lecuyer_cmrg_seed_stream repeatable
 
     Code
-      withr::with_seed(10, get_seed_stream())
+      withr::with_seed(10, get_lecuyer_cmrg_seed_stream())
     Output
       [[1]]
       [1]       10407 -1852613690  1473416771  -413238268    -5614164   596817356
       [7] -2081937358
       
 
-# get_seed_stream repeatable multiple seeds
+# get_lecuyer_cmrg_seed_stream repeatable multiple seeds
 
     Code
-      withr::with_seed(10, get_seed_stream(nseeds = 2L))
+      withr::with_seed(10, get_lecuyer_cmrg_seed_stream(nseeds = 2L))
     Output
       [[1]]
       [1]       10407 -1852613690  1473416771  -413238268    -5614164   596817356
@@ -21,20 +21,20 @@
       [1]      10407 -523009245 2009368735  379397622   26024764 -450628420 -210045713
       
 
-# get_seed_stream repeatable other starts
+# get_lecuyer_cmrg_seed_stream repeatable other starts
 
     Code
-      withr::with_seed(42, get_seed_stream())
+      withr::with_seed(42, get_lecuyer_cmrg_seed_stream())
     Output
       [[1]]
       [1]       10407  1812818762   957998414 -1062960512   738666160 -1560237390
       [7]  1852499593
       
 
-# get_seed_stream seeds repeatable with other seed types
+# get_lecuyer_cmrg_seed_stream seeds repeatable with other seed types
 
     Code
-      with_lecuyer_cmrg_seed(10, get_seed_stream())
+      with_lecuyer_cmrg_seed(10, get_lecuyer_cmrg_seed_stream())
     Output
       [[1]]
       [1]       10407   115614581  1315917716   768866738 -1769155862  1955826369
