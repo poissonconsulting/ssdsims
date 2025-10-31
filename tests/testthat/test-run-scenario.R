d5a451725f146a08a4f6df67bd616be80bc0b6c5
Joe Thorley <joe@poissonconsulting.ca>
2025-10-31 10:34:16 -0700

    update to list list

--- tests/testthat/test-run-scenario.R
+++ tests/testthat/test-run-scenario.R
@@ -2,12 +2,9 @@ test_that("ssd_run_scenario.data.frame works", {
   with_lecuyer_cmrg_seed(10, {
     scenario <- ssd_run_scenario(ssddata::ccme_boron, replace = c(TRUE, FALSE), nsim = 2)
   })
-  scenario$fits <- NULL
   
   scenario <- tidyr::unnest(scenario, cols = data)
    expect_snapshot_data(scenario, "scenarioTF2_data")
-
-  scenario$data <- NULL
 
   scenario <- tidyr::unnest(scenario, cols = hc)
    expect_snapshot_data(scenario, "scenarioTF2")
@@ -33,13 +30,9 @@ test_that("ssd_run_scenario.function works", {
   with_lecuyer_cmrg_seed(10, {
     scenario <- ssd_run_scenario(rlnorm, nsim = 2)
   })
-  scenario$fits <- NULL
 
   scenario <- tidyr::unnest(scenario, cols = data)
    expect_snapshot_data(scenario, "scenarioFun2_data")
-
-    scenario$data <- NULL
-
 
   scenario <- tidyr::unnest(scenario, cols = hc)
    expect_snapshot_data(scenario, "scenarioFun2")
@@ -49,13 +42,9 @@ test_that("ssd_run_scenario.character works", {
   with_lecuyer_cmrg_seed(10, {
     scenario <- ssd_run_scenario("rlnorm", nsim = 2)
   })
-  scenario$fits <- NULL
-
 
   scenario <- tidyr::unnest(scenario, cols = data)
    expect_snapshot_data(scenario, "scenarioStr2_data")
-
-  scenario$data <- NULL
   
   scenario <- tidyr::unnest(scenario, cols = hc)
    expect_snapshot_data(scenario, "scenarioStr2")
@@ -66,12 +55,9 @@ test_that("ssd_run_scenario.tmbfit works", {
     fit <- ssdtools::ssd_fit_dists(ssddata::ccme_boron) 
     scenario <- ssd_run_scenario(fit[[1]], nsim = 2)
   })
-  scenario$fits <- NULL
-    
+  
   scenario <- tidyr::unnest(scenario, cols = data)
    expect_snapshot_data(scenario, "scenarioTMB2_data")
-
-  scenario$data <- NULL
   
   scenario <- tidyr::unnest(scenario, cols = hc)
    expect_snapshot_data(scenario, "scenarioTMB2")
@@ -83,12 +69,9 @@ test_that("ssd_run_scenario.fitdist works", {
     fit <- ssdtools::ssd_fit_dists(ssddata::ccme_boron) 
     scenario <- ssd_run_scenario(fit, dist_sim = c("lnorm", "top"), nsim = 2)
   })
-  scenario$fits <- NULL
 
   scenario <- tidyr::unnest(scenario, cols = data)
    expect_snapshot_data(scenario, "scenarioFit2_data")
-
-    scenario$data <- NULL
 
   scenario <- tidyr::unnest(scenario, cols = hc)
    expect_snapshot_data(scenario, "scenarioFit2")
