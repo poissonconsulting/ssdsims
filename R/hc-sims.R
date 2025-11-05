ccfd54fd9e512ac0661e64ae1ed02b1be6041252
Joe Thorley <joe@poissonconsulting.ca>
2025-11-05 13:13:06 -0800

    - Vectorize `ssd_hc_sims(ci_method)`
    closes Vectorize arguments for `ssd_hcs_sims()`
    Fixes #33

--- R/hc-sims.R
+++ R/hc-sims.R
@@ -6,14 +6,24 @@
 #' @param ... Additional arguments passed to `ssdtools::ssd_hc()`.
 #' @return The x tibble with a list column hc of data frames produced by applying ssd_hc() to fits.
 #' @export
-ssd_hc_sims <- function(x, proportion = 0.05, ..., ci = FALSE, seed = NULL, save_to = NULL, .progress = FALSE) {
+ssd_hc_sims <- function(x, proportion = 0.05, ..., ci = FALSE, ci_method = "weighted_samples", seed = NULL, save_to = NULL, .progress = FALSE) {
   chk::check_data(x, values = list(sim = c(1L, 10000000L), stream = c(1L, 10000000L)))
   chk::check_names(x, "fits")
   chk::chk_null_or(save_to, vld = chk::vld_dir)
 
+  chk::chk_character(ci_method)
+  chk::chk_not_any_na(ci_method)
+  chk::chk_unique(ci_method)
+  chk::chk_subset(ci_method, ssdtools::ssd_ci_methods())
+  chk::chk_length(ci_method, upper = Inf)
+
   if(!nrow(x)) {
     return(dplyr::mutate(x, hc = list()))
   }
-  x$hc <- purrr::pmap(list(x$fits, x$sim, x$stream), \(.x, .sim, .stream) hc_seed(.x, .sim, .stream, seed = seed, proportion = proportion, save_to = save_to, ci = ci,...), .progress = .progress)
+
+  x <- x |>
+    dplyr::cross_join(tidyr::expand_grid(ci_method = ci_method))
+
+  x$hc <- purrr::pmap(list(x$fits, x$sim, x$stream, x$ci_method), \(.x, .sim, .stream, .ci_method) hc_seed(.x, .sim, .stream, ci_method = .ci_method, seed = seed, proportion = proportion, save_to = save_to, ci = ci,...), .progress = .progress)
   x
 }
--- R/internal.R
+++ R/internal.R
@@ -25,16 +25,16 @@ fit_dists_seed <- function(data, sim, stream, seed, dists, silent, ...) {
   fit
 }
 
-hc_seed <- function(data, sim, stream, seed, proportion, ci, save_to, ...) {
+hc_seed <- function(data, sim, stream, ci_method, seed, proportion, ci, save_to, ...) {
   seed <- get_lecuyer_cmrg_seed_stream(seed = seed, start_sim = sim, stream = stream)
   ## TODO: handle failures
   with_lecuyer_cmrg_seed(seed, {
-    fit <- ssdtools::ssd_hc(data, proportion = proportion, ci = ci, ...)
+    hc<- ssdtools::ssd_hc(data, proportion = proportion, ci = ci, ci_method = ci_method, ...)
   })
-  fit 
+  dplyr::select(hc, !"ci_method") 
 }
 
-run_scenario <- function(x, ..., dists = dists, proportion = proportion, ci = ci, .progress = .progress) {
+run_scenario <- function(x, ..., dists, proportion, ci, ci_method, .progress = .progress) {
   .args <- list(...)
 
   fit_dists_formals <- methods::formalArgs(ssdtools::ssd_fit_dists)
@@ -55,7 +55,7 @@ run_scenario <- function(x, ..., dists = dists, proportion = proportion, ci = ci
 
   x <- do.call("ssd_fit_dists_sims", .args_fit)
 
-  .args_hc <- list(x = x, proportion = proportion, ci = ci, .progress = .progress) |>
+  .args_hc <- list(x = x, proportion = proportion, ci = ci, ci_method = ci_method, .progress = .progress) |>
     c(.args_hc)
 
   do.call("ssd_hc_sims", .args_hc)
