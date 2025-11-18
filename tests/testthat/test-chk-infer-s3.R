c93aa9560d3e391f2218121513bfae625cea06d7
Kirill Müller <kirill@cynkra.com>
2025-11-18 23:01:39 +0100

    S3

---
+++ tests/testthat/test-chk-infer-s3.R
@@ -0,0 +1,327 @@
+test_that("contains_use_method detects UseMethod call", {
+  expr1 <- quote(UseMethod("foo"))
+  expect_true(contains_use_method(expr1))
+
+  expr2 <- quote({
+    x <- 1
+    UseMethod("foo")
+  })
+  expect_true(contains_use_method(expr2))
+
+  expr3 <- quote(print(x))
+  expect_false(contains_use_method(expr3))
+})
+
+test_that("find_use_method extracts generic name", {
+  expr1 <- quote(UseMethod("foo"))
+  expect_equal(find_use_method(expr1), "foo")
+
+  expr2 <- quote({
+    x <- 1
+    UseMethod("bar")
+  })
+  expect_equal(find_use_method(expr2), "bar")
+
+  expr3 <- quote(print(x))
+  expect_null(find_use_method(expr3))
+})
+
+test_that("is_s3_generic identifies S3 generics", {
+  # Create a simple S3 generic
+  test_generic <- function(x, ...) {
+    UseMethod("test_generic")
+  }
+
+  expect_true(is_s3_generic(test_generic))
+
+  # Regular function
+  regular_fun <- function(x) {
+    x + 1
+  }
+
+  expect_false(is_s3_generic(regular_fun))
+})
+
+test_that("collect_method_args collects unique arguments", {
+  method1 <- function(x, y, z) NULL
+  method2 <- function(x, a, b) NULL
+  method3 <- function(x, y, c) NULL
+
+  method_funs <- list(
+    method1 = method1,
+    method2 = method2,
+    method3 = method3
+  )
+
+  args <- collect_method_args(method_funs)
+  expect_setequal(args, c("x", "y", "z", "a", "b", "c"))
+})
+
+test_that("combine_method_results deduplicates expressions", {
+  # Create mock results with duplicate expressions
+  expr1 <- quote(chk_not_null(x))
+  expr2 <- quote(chk_not_null(y))
+  expr3 <- quote(chk_not_null(x)) # Duplicate of expr1
+
+  results_list <- list(
+    list(x = list(expr1), y = list(expr2)),
+    list(x = list(expr3), y = list())
+  )
+
+  fun_args <- c("x", "y")
+
+  combined <- combine_method_results(results_list, fun_args)
+
+  expect_length(combined$x, 1) # Should deduplicate
+  expect_length(combined$y, 1)
+  expect_identical(combined$x[[1]], expr1)
+  expect_identical(combined$y[[1]], expr2)
+})
+
+test_that("analyze_s3_generic works with mocked extract_chk_calls", {
+  # Create a simple S3 generic with methods
+  # Need to assign to global environment for methods() to find them
+  local({
+    test_fun <<- function(x, y, ...) {
+      UseMethod("test_fun")
+    }
+
+    test_fun.default <<- function(x, y, z = 1) {
+      x + y + z
+    }
+
+    test_fun.numeric <<- function(x, y, a = 2) {
+      x * y + a
+    }
+  })
+
+  # Clean up after test
+  on.exit(
+    {
+      rm(test_fun, test_fun.default, test_fun.numeric, envir = .GlobalEnv)
+    },
+    add = TRUE
+  ) # Mock extract_chk_calls to return simple chk_not_null calls
+  mock_extract_chk_calls <- function(
+    fun,
+    arg = NULL,
+    fun_name = NULL,
+    .depth = 0
+  ) {
+    fun_args <- names(formals(fun))
+    fun_args <- fun_args[fun_args != "..."]
+
+    if (!is.null(arg)) {
+      fun_args <- arg
+    }
+
+    result <- setNames(
+      lapply(fun_args, function(arg_name) {
+        list(call("chk_not_null", as.symbol(arg_name)))
+      }),
+      fun_args
+    )
+
+    result
+  }
+
+  # Test analyze_s3_generic with the mock
+  result <- analyze_s3_generic(
+    fun = test_fun,
+    arg = NULL,
+    fun_name = "test_fun",
+    indent = "",
+    .depth = 0,
+    extract_chk_calls = mock_extract_chk_calls
+  )
+
+  # Should have collected arguments from both methods
+  expect_true("x" %in% names(result))
+  expect_true("y" %in% names(result))
+  expect_true("z" %in% names(result))
+  expect_true("a" %in% names(result))
+
+  # Each argument should have a chk_not_null call
+  expect_length(result$x, 1)
+  expect_equal(result$x[[1]], quote(chk_not_null(x)))
+
+  expect_length(result$y, 1)
+  expect_equal(result$y[[1]], quote(chk_not_null(y)))
+
+  expect_length(result$z, 1)
+  expect_equal(result$z[[1]], quote(chk_not_null(z)))
+
+  expect_length(result$a, 1)
+  expect_equal(result$a[[1]], quote(chk_not_null(a)))
+})
+
+test_that("analyze_s3_generic with specific argument", {
+  # Create a simple S3 generic with methods
+  local({
+    test_fun <<- function(x, y, ...) {
+      UseMethod("test_fun")
+    }
+
+    test_fun.default <<- function(x, y, z = 1) {
+      x + y + z
+    }
+
+    test_fun.numeric <<- function(x, y, a = 2) {
+      x * y + a
+    }
+  })
+
+  # Clean up after test
+  on.exit(
+    {
+      rm(test_fun, test_fun.default, test_fun.numeric, envir = .GlobalEnv)
+    },
+    add = TRUE
+  ) 
+  
+  # Mock extract_chk_calls
+  mock_extract_chk_calls <- function(
+    fun,
+    arg = NULL,
+    fun_name = NULL,
+    .depth = 0
+  ) {
+    fun_args <- names(formals(fun))
+    fun_args <- fun_args[fun_args != "..."]
+
+    if (!is.null(arg)) {
+      fun_args <- arg
+    }
+
+    result <- setNames(
+      lapply(fun_args, function(arg_name) {
+        list(call("chk_not_null", as.symbol(arg_name)))
+      }),
+      fun_args
+    )
+
+    result
+  }
+
+  rlang::local_options(cli.default_handler = invisible)
+
+  # Test with specific argument "x"
+  result <- analyze_s3_generic(
+    fun = test_fun,
+    arg = "x",
+    fun_name = "test_fun",
+    indent = "",
+    .depth = 0,
+    extract_chk_calls = mock_extract_chk_calls
+  )
+
+  # Should only have results for "x"
+  expect_equal(names(result), "x")
+  expect_length(result$x, 1)
+  expect_equal(result$x[[1]], quote(chk_not_null(x)))
+})
+
+test_that("analyze_s3_generic skips methods without requested argument", {
+  # Create a simple S3 generic with methods
+  local({
+    test_fun <<- function(x, ...) {
+      UseMethod("test_fun")
+    }
+
+    test_fun.default <<- function(x, y) {
+      x + y
+    }
+
+    test_fun.numeric <<- function(x, z) {
+      # Has 'z' instead of 'y'
+      x * z
+    }
+  })
+
+  # Clean up after test
+  on.exit(
+    {
+      rm(test_fun, test_fun.default, test_fun.numeric, envir = .GlobalEnv)
+    },
+    add = TRUE
+  )
+
+  # Mock extract_chk_calls
+  mock_extract_chk_calls <- function(
+    fun,
+    arg = NULL,
+    fun_name = NULL,
+    .depth = 0
+  ) {
+    fun_args <- names(formals(fun))
+
+    if (!is.null(arg)) {
+      if (!(arg %in% fun_args)) {
+        # This method doesn't have the requested argument
+        result <- list()
+        result[[arg]] <- list()
+        return(result)
+      }
+      fun_args <- arg
+    }
+
+    result <- setNames(
+      lapply(fun_args, function(arg_name) {
+        list(call("chk_not_null", as.symbol(arg_name)))
+      }),
+      fun_args
+    )
+
+    result
+  }
+
+  rlang::local_options(cli.default_handler = invisible)
+
+  # Test with argument "y" which only exists in default method
+  result <- analyze_s3_generic(
+    fun = test_fun,
+    arg = "y",
+    fun_name = "test_fun",
+    indent = "",
+    .depth = 0,
+    extract_chk_calls = mock_extract_chk_calls
+  )
+
+  # Should have results for "y" only from the default method
+  expect_equal(names(result), "y")
+  expect_length(result$y, 1)
+  expect_equal(result$y[[1]], quote(chk_not_null(y)))
+})
+
+test_that("get_s3_methods retrieves methods correctly", {
+  # Create a simple S3 generic with methods
+  local({
+    test_gen <<- function(x, ...) {
+      UseMethod("test_gen")
+    }
+
+    test_gen.default <<- function(x, y) {
+      x + y
+    }
+
+    test_gen.numeric <<- function(x, z) {
+      x * z
+    }
+  })
+
+  # Clean up after test
+  on.exit(
+    {
+      rm(test_gen, test_gen.default, test_gen.numeric, envir = .GlobalEnv)
+    },
+    add = TRUE
+  )
+
+  methods_list <- get_s3_methods(test_gen)
+
+  expect_type(methods_list, "list")
+  expect_true("test_gen.default" %in% names(methods_list))
+  expect_true("test_gen.numeric" %in% names(methods_list))
+  expect_true(is.function(methods_list$test_gen.default))
+  expect_true(is.function(methods_list$test_gen.numeric))
+})
