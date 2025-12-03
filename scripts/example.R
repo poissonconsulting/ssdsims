3ba8b8c935b8961cac77b9b267ec76189e7b0de3
Joe Thorley <joe@poissonconsulting.ca>
2025-12-03 10:35:37 -0800

    update example

--- scripts/example.R
+++ scripts/example.R
@@ -1,4 +1,5 @@
 ## Example scenario
+## I don't need samples = TRUE necessarily, but I am keen to understand the mechanism by which these could be kept.
 
 library(ssdsims)
 
@@ -8,13 +9,15 @@ hcs <- ssd_hc_sims(fits)
 print(hcs)
 
 ssd_run_scenario(ssddata::ccme_boron, 
-                 nsim=2L,
-                 nrow=c(5L, 6L, 10L, 20L, 50L),
+                 nsim = 2L,
+                 nrow = c(5L, 6L, 10L, 20L, 50L),
                  proportion = c(0.01, 0.05, 0.1, 0.2),
-                 ci=TRUE, 
-                 ci_method=c("multi_fixed","multi_free", "weighted_samples", "MACL"),
-                 est_method=c("multi", "arithmetic", "geometric"),
-                 parametric=c(TRUE, FALSE),
+                 ci_method = c("arithmetic_samples", "geometric_samples", "GMACL", "GMAW1", 
+                 "GMAW2", "MACL", "MAW1", "MAW2", "multi_fixed", "multi_free", 
+                 "weighted_samples"),
                  nboot=c(1, 5, 10, 50, 100, 500), # * 100,
+                 ci = TRUE, 
+                 est_method = "multi",
+                 parametric = TRUE,
                  samples=TRUE,
                  delta=Inf)
